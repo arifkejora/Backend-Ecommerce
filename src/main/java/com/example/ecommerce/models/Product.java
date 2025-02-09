8d5a3ace066b2a7ab70a3c1fd1ea4c1eed837063
arif <widiartoarif55@gmail.com>
2025-02-09 19:42:16 +0700

    Set Up Backend Ecommerce Insert & Get List Product

---
+++ src/main/java/com/example/ecommerce/models/Product.java
@@ -0,0 +1,19 @@
+package com.example.ecommerce.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private double price;
+}
